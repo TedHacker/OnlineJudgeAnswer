47d3cbc7b947d2f958c9ca277f6296e1433556f5
TedHacker <254467996@qq.com>
2015-08-15 12:58:47 +0800

    add

--- src/main/java/leetcode/MinimumPathSum.java
+++ src/main/java/leetcode/MinimumPathSum.java
@@ -6,28 +6,21 @@ package leetcode;
  */
 public class MinimumPathSum {
     public class Solution {
-        int n, m;
-        int minx;
-        int[][] mapx;
-
-        private void dfs(int x, int y, int sumx) {
-            if (x == n - 1 && y == m - 1) {
-                minx = Math.min(minx, sumx + mapx[x][y]);
-                return;
-            }
-            if (x >= n || y >= m) return;
-            dfs(x + 1, y, sumx + mapx[x][y]);
-            dfs(x, y + 1, sumx + mapx[x][y]);
-        }
-
         public int minPathSum(int[][] grid) {
             if (grid == null) return 0;
-            n = grid.length;
-            m = grid[0].length;
-            minx = Integer.MIN_VALUE;
-            mapx = grid;
-            dfs(0, 0, 0);
-            return minx;
+            int n = grid.length;
+            int m = grid[0].length;
+            int[][] dp = new int[n][m];
+            dp[0][0] = grid[0][0];
+            for (int i = 1; i < n; ++i)
+                dp[i][0] = grid[i][0] + dp[i - 1][0];
+            for (int i = 1; i < m; ++i)
+                dp[0][i] = grid[0][i] + dp[0][i - 1];
+            for (int i = 1; i < n; ++i)
+                for (int j = 1; j < m; ++j) {
+                    dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - 1]) + grid[i][j];
+                }
+            return dp[n - 1][m - 1];
         }
     }
 }
--- src/main/java/leetcode/RotateImage.java
+++ src/main/java/leetcode/RotateImage.java
@@ -10,13 +10,24 @@ public class RotateImage {
             if (matrix == null) return;
             int tmp;
             int n = matrix.length;
+            int mid = n / 2;
             for (int i = 0; i < n / 2; ++i) {
                 for (int j = 0; j < n / 2; ++j) {
                     tmp = matrix[i][j];
-                    matrix[i][j] = matrix[n - 1 - i][j];
-                    matrix[n - 1 - i][j] = matrix[n - 1 - i][n - 1 - j];
-                    matrix[n - 1 - i][n - 1 - j] = matrix[i][n - 1 - j];
-                    matrix[i][n - 1 - j] = tmp;
+                    matrix[i][j] = matrix[n - 1 - j][i];
+                    matrix[n - 1 - j][i] = matrix[n - 1 - i][n - 1 - j];
+                    matrix[n - 1 - i][n - 1 - j] = matrix[j][n - 1 - i];
+                    matrix[j][n - 1 - i] = tmp;
+                }
+            }
+            if ((n & 1) > 0) {//当行列数为奇数时
+
+                for (int i = 0; i <= mid; ++i) {
+                    tmp = matrix[i][mid];
+                    matrix[i][mid] = matrix[mid][i];
+                    matrix[mid][i] = matrix[n - 1 - i][mid];
+                    matrix[n - 1 - i][mid] = matrix[mid][n - 1 - i];
+                    matrix[mid][n - 1 - i] = tmp;
                 }
             }
             return;
